607e22cfe53e30c8551d3868dece022fffdf4a63
ofurkand <omerfurkandurmus@gmail.com>
2025-01-26 05:03:41 +0300

    Düzenlemeler

--- CalismaAlani/denemeler/KartGosterimi/script.js
+++ CalismaAlani/denemeler/KartGosterimi/script.js
@@ -1,18 +1,19 @@
-function fetchData(url) {
-    return fetch(url)
-      .then(response => response.json()) // Convert the response to JSON
-      .then(data => {
-        return data; // Return the data as an object/dictionary
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }
+// function fetchData(url) {
+//     return fetch(url)
+//       .then(response => response.json()) 
+//       .then(data => {
+//         return data; 
+//       })
+//       .catch(error => {
+//         console.error('Error fetching data:', error);
+//       });
+//   }
 
-let myData; // Declare the variable outside of the function
+// let myData; 
 
-const url = '../KartGosterimi/varliklar/veriler/ana_veriler.json'; // Sample API
-fetchData(url).then(data => {
-  myData = data; // Store the fetched data into the variable
-  console.log(myData); // `myData` now contains the fetched object
-});
+// fetchData(url).then(data => {
+//   myData = data; 
+//   console.log(myData); 
+// });
+
+const kaynakURL = '../KartGosterimi/varliklar/veriler/ana_veriler.json'; 
