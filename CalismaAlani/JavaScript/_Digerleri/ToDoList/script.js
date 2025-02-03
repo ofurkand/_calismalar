4aad0ed81eeaa6a25bc695ca8b21ec852dce0a1b
ofurkand <omerfurkandurmus@gmail.com>
2025-02-03 21:42:18 +0300

    bugifx

--- CalismaAlani/JavaScript/_Digerleri/ToDoList/script.js
+++ CalismaAlani/JavaScript/_Digerleri/ToDoList/script.js
@@ -47,32 +47,39 @@ function loadToDoList(){
         todos.forEach(element => {
             todo_list.appendChild(element.uret());
         });
-    }
-    checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(x=>
+        checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxesEventListener();
+        checkboxes.forEach(x=>
         x.checked ? x.parentElement.getElementsByTagName('span')[0].style.textDecoration= 'line-through':
         x.parentElement.getElementsByTagName('span')[0].style.textDecoration= ''
     );
+    }
 }
 
-async function checkboxesEventListener(){
-    await checkboxes.forEach(x=>x.addEventListener('click',()=>{
-        let span = x.parentElement.getElementsByTagName('span')[0];
-        if(x.checked){
-            span.style.textDecoration= 'line-through';
+function checkboxesEventListenerCode(){
+    let span = this.parentElement.getElementsByTagName('span')[0];
+    if(this.checked){
+        span.style.textDecoration= 'line-through';
+    }
+    else{
+        span.style.textDecoration= '';
+    }
+    let cnt = 0;
+    Array.from(todo_list.children).forEach(element => {
+        if(element === span.parentElement){
+            todos[cnt].yapildiMi = !todos[cnt].yapildiMi;
         }
-        else{
-            span.style.textDecoration= '';
-        }
-        let cnt = 0;
-        Array.from(todo_list.children).forEach(element => {
-            if(element === span.parentElement){
-                todos[cnt].yapildiMi = !todos[cnt].yapildiMi;
-            }
-            cnt++;
-        })
-        saveToDoList();
-    }));
+        cnt++;
+    })
+    saveToDoList();
+}
+
+function checkboxesEventListener(){    
+    checkboxes.forEach(x => {
+        x.removeEventListener('click', checkboxesEventListenerCode);
+        x.addEventListener('click', checkboxesEventListenerCode);
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -88,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
 addButton.addEventListener("click",()=>{
     if (main_input.value.trim() != '') {
         todos.push(new YapilacakGorev(false, main_input.value));
-        localStorage.setItem('todos',JSON.stringify(todos));
+        saveToDoList();
         loadToDoList();
         main_input.value='';
         app.style.transition = 'opacity 0.250s';
