c83339d67b947e3cb7ac97e10c8b381195d4d70b
Ömer Furkan <omerfurkandurmus@gmail.com>
2025-02-12 17:45:50 +0300

    go

--- yardimlar/go/Tutorials/A_Tour_of_Go/Doc/general.go
+++ yardimlar/go/Tutorials/A_Tour_of_Go/Doc/general.go
@@ -236,11 +236,11 @@
 // func modExp(base, exp int, mod int) int {
 // 	result := 1
 // 	for exp > 0 {
-// 		if exp%2 == 1 { // Eğer üs tekse
+// 		if exp%2 == 1 {
 // 			result = (result * base) % mod
 // 		}
-// 		base = (base * base) % mod // Tabanı karesini alıyoruz
-// 		exp /= 2                   // Üssü yarıya indiriyoruz
+// 		base = (base * base) % mod
+// 		exp /= 2
 // 	}
 // 	return result
 // }
@@ -262,37 +262,185 @@
 // 	fmt.Println(modExp2(base, exp))     // Çıktı: 1
 // }
 
-// pointer-kullanimi
+// daha sonra kullanılacak // pointer-kullanimi
+// package main
+
+// import "fmt"
+
+// // Bir işaretçi parametre kabul eden bir fonksiyon
+// func printPointerValue(ptr *int) {
+// 	if ptr == nil {
+// 		fmt.Println("Pointer is nil!")
+// 	} else {
+// 		fmt.Println("Pointer is not nil, value:", *ptr)
+// 	}
+// }
+
+// func main() {
+// 	var ptr *int // ptr işaretçi (pointer) değişkeni, başlangıçta nil (yani işaretçi bir değeri göstermiyor)
+
+// 	fmt.Println(ptr) // nil yazdırır
+
+// 	// nil kontrolü
+// 	if ptr == nil {
+// 		fmt.Println("Pointer nil")
+// 	}
+
+// 	// Bir işaretçiye değer atama
+// 	value := 10
+// 	ptr = &value // ptr işaretçisini value değişkenine işaret edecek şekilde ayarlıyoruz
+
+// 	fmt.Println(ptr)  // Bellek adresini yazdırır
+// 	fmt.Println(*ptr) // İşaret edilen değeri yazdırır (10)
+
+// 	var ptr2 *int           // İşaretçi başlangıçta nil
+// 	printPointerValue(ptr2) // Nil işaretçi gönderiyoruz
+// }
+
+// for.go // while dahilî
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	sum := 0
+// 	// for i := 0; i < 10; i++ {
+// 	// 	sum += i
+// 	// }
+
+// 	// for ; sum < 1000; {
+// 	for sum < 1000 {
+// 		sum += sum
+// 	}
+// 	fmt.Println(sum)
+// }
+
+// forever.go
+// package main
+
+// func main() {
+// 	for {
+// 	}
+// }
+
+// if-and-else.go
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// func sqrt(x float64) string {
+// 	if x < 0 {
+// 		return sqrt(-x) + "i"
+// 	}
+// 	// return fmt.Sprint(math.Sqrt(x))
+// 	return fmt.Sprint(math.Sqrt(x))
+// }
+
+// func pow(x, n, lim float64) float64 {
+// 	if v := math.Pow(x, n); v < lim {
+// 		return v
+// 	} else {
+// 		fmt.Printf("%g >= %g\n", v, lim)
+// 	}
+// 	// can't use v here, though
+// 	return lim
+// 	// lim, sonucun geçmemesi gereken bir üst sınır olması
+// 	// adına iş görüyor. Büyük sayıların bellekeği zorlaması
+// 	// konusu hafifletilmektedir. lim => limit
+// }
+
+// func main() {
+// 	fmt.Println(sqrt(2), sqrt(-4))
+// 	fmt.Println(
+// 		pow(3, 2, 10),
+// 		pow(3, 3, 20),
+// 	)
+// }
+
+// switch.go
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// )
+
+// func main() {
+// 	fmt.Print("Go runs on ")
+// 	// os := runtime.GOOS
+// 	// switch os {
+// 	switch os := runtime.GOOS; os {
+// 	case "darwin":
+// 		fmt.Println("OS X.")
+// 	case "linux":
+// 		fmt.Println("Linux.")
+// 	default:
+// 		// freebsd, openbsd,
+// 		// plan9, windows...
+// 		fmt.Printf("%s.\n", os)
+// 	}
+// }
+
+// switch-with-no-condition.go
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	t := time.Now()
+// 	switch {
+// 	case t.Hour() < 12:
+// 		fmt.Println("Good morning!")
+// 	case t.Hour() < 17:
+// 		fmt.Println("Good afternoon.")
+// 	default:
+// 		fmt.Println("Good evening.")
+// 	}
+// }
+
+// switch-evaluation-order.go
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	fmt.Println("When's Saturday?")
+// 	today := time.Now().Weekday()
+// 	switch time.Saturday {
+// 	case today + 0:
+// 		fmt.Println("Today.")
+// 	case today + 1:
+// 		fmt.Println("Tomorrow.")
+// 	case today + 2:
+// 		fmt.Println("In two days.")
+// 	default:
+// 		fmt.Println("Too far away.")
+// 	}
+// }
+
+// defer.go
 package main
 
 import "fmt"
 
-// Bir işaretçi parametre kabul eden bir fonksiyon
-func printPointerValue(ptr *int) {
-	if ptr == nil {
-		fmt.Println("Pointer is nil!")
-	} else {
-		fmt.Println("Pointer is not nil, value:", *ptr)
-	}
-}
-
 func main() {
-	var ptr *int // ptr işaretçi (pointer) değişkeni, başlangıçta nil (yani işaretçi bir değeri göstermiyor)
+	defer fmt.Println("world")
+	defer fmt.Println("hello")
 
-	fmt.Println(ptr) // nil yazdırır
+	fmt.Println("counting")
 
-	// nil kontrolü
-	if ptr == nil {
-		fmt.Println("Pointer nil")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
 	}
 
-	// Bir işaretçiye değer atama
-	value := 10
-	ptr = &value // ptr işaretçisini value değişkenine işaret edecek şekilde ayarlıyoruz
-
-	fmt.Println(ptr)  // Bellek adresini yazdırır
-	fmt.Println(*ptr) // İşaret edilen değeri yazdırır (10)
-
-	var ptr2 *int           // İşaretçi başlangıçta nil
-	printPointerValue(ptr2) // Nil işaretçi gönderiyoruz
+	fmt.Println("done")
 }
