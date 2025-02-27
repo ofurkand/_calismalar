be5532701df79522fc891463254ae8b285b0b918
ofurkand <omerfurkandurmus@gmail.com>
2025-02-27 17:40:06 +0300

    svelte giriş

---
+++ CalismaAlani/svelte/Syntax/spa/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<script>
+    import Header from './Header.svelte';
+</script>
+
+<Header name="Furkan" fake_name="Wes"/>
+
+<button onclick={() => onclicck() }>Toggle Status</button>
+
+<p>The store is now {status}</p>
---
+++ CalismaAlani/svelte/Syntax/spa/src/routes/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let {name,fake_name }:{
+        name: string;
+        fake_name?: string | null;
+    } =  $props();    
+    let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+    function onclicck(){
+        status = status === "OPEN" ? "CLOSED": "OPEN";
+    };
+
+</script>
+
+<h1>HEADER {name}{status}</h1>
+
+<input type="text" bind:value={name}>
+
+{fake_name}
+
+<style>
+
+</style>
