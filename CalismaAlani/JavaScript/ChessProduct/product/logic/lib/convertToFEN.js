0cd4e80438aa0156891a47c8c326d55b31ef6c0a
ofurkand <omerfurkandurmus@gmail.com>
2025-03-13 15:15:28 +0300

    ChessProduct has advanced to be handled as a separate project in a different repo

--- CalismaAlani/JavaScript/ChessProduct/product/logic/lib/convertToFEN.js
+++
@@ -1,5 +0,0 @@
-export function formatFEN(moves,FEN,startPosition,gameArea){
-    if (startPosition != null && gameArea != null && (moves === null && FEN === null)) {
-        return [startPosition[0]]
-    }
-}
--- CalismaAlani/JavaScript/ChessProduct/product/logic/lib/moves/pos-L.js
+++
@@ -1,7 +0,0 @@
-import { bottomRight,bottomLeft,topLeft,topRight } from "./diagonal.js";
-import { downward,upward,right,left } from "./horizontal.js";
-
-// - değer taşa göre sol, + değerler taşa göre sağ tarafa ne kadar ilerleyeceğidir.
-export function positionL(pieceArrayCoord,solvedFEN,turn = true,){
-    
-}
