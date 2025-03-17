490b7f4823b90aae27c25ab598636cf807c8fdcf
ofurkand <omerfurkandurmus@gmail.com>
2025-03-17 17:59:49 +0300

    to be continued

---
+++ CalismaAlani/svelte/musicplayerdemo/src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	// export let name;
+	import {onMount} from "svelte"
+    import MusicList from "./musiclist.svelte";
+	
+	let currentSongIndex = 0;
+	let playerState = "play";
+	let audioElement;
+	let mainElement;
+</script>
+
+<main bind:this={mainElement}>
+	<!-- <h1>Hello {name}!</h1> -->
+	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+	<audio src={"../public/storage/audio/"+ $MusicList[currentSongIndex]}>
+	</audio>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	/* h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	} */
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ CalismaAlani/svelte/musicplayerdemo/src/musiclist.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { writable } from "svelte/store";
+    export const MusicList = writable(
+        [
+            {
+                audio:"DOU - Aşk-ı Kıyamet (Official Audio).mp3",
+                image:"hqdefault.png",
+                artist:"DOU"
+            }, 
+            {
+                audio:"kazım koyuncu şevval sam'ben seni sevduğumi'.mp3",
+                image:"videoframe_8477.png",
+                artist:"Kazım Koyuncu"
+            }, 
+            {
+                audio:"Mahşer [Official Video] - Gökhan Türkmen #Mahşer.mp3",
+                image:"videoframe_1686.png",
+                artist:"Gökhan Türkmen"
+            },
+        ]
+    );
+</script>
+
